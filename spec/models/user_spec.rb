5025aa2f8437e623579bd7cdf85923ea4987ec36
David Auza <davidauza01@gmail.com>
2021-11-07 13:19:55 -0500

    Add models tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:recipes).dependent(:destroy) }
+  end
+
+  describe 'scopes' do
+    describe '#recent' do
+      subject { described_class.recent
+      }
+      let!(:user_one) { described_class.create }
+      let!(:user_two) { described_class.create }
+
+      it { is_expected.to match_array([user_two, user_one]) }
+    end
+  end
 end
