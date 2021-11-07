c36050f278c42bb9893231102e46cb8fb820ae3b
David Auza <davidauza01@gmail.com>
2021-11-07 16:19:49 -0500

    Update spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe User, type: :model do
 
   describe 'scopes' do
     describe '#recent' do
-      subject { described_class.recent
-      }
+      subject { described_class.recent }
+
       let!(:user_one) { described_class.create }
       let!(:user_two) { described_class.create }
 
