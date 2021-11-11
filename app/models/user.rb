e5d0aa1e24172cac48eb40fdbad882272b43e93d
David Auza <davidauza01@gmail.com>
2021-11-11 09:27:22 -0500

    Update app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@
 
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
-  scope :recent, -> { order('users.created_at DESC') }
+  scope :recent, -> { order(created_at: :desc) }
   scope :single_recipe_authors, lambda { |date_from: 1.year.ago, date_to: Time.current, page: nil|
                                   joins(:recipes)
-                                    .where('recipes.published_at BETWEEN ? AND ?', date_from, date_to)
+                                    .where(recipes: { published_at: date_from...date_to })
                                     .group(:id)
                                     .having('COUNT(users.id) = 1')
                                     .recent
