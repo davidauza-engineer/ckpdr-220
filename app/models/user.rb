204a0f464a13aced1a494ec800a26f03a4f50605
David Auza <davidauza01@gmail.com>
2021-11-11 09:37:54 -0500

    Update single_recipe_authors scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   scope :recent, -> { order(created_at: :desc) }
-  scope :single_recipe_authors, lambda { |date_from: 1.year.ago, date_to: Time.current, page: nil|
+  scope :single_recipe_authors, (lambda do |date_from: 1.year.ago, date_to: Time.current, page: nil|
                                   joins(:recipes)
                                     .where(recipes: { published_at: date_from...date_to })
                                     .group(:id)
                                     .having('COUNT(users.id) = 1')
                                     .recent
                                     .page(page)
-                                }
+                                end)
   EMAIL_REGEXP = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/.freeze
   validates :email, format: { with: EMAIL_REGEXP, message: 'Only allows valid email addresses.' },
                     length: { minimum: 5, max: 255 }, uniqueness: true
